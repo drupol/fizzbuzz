414c40430a9ed8602a7b9e62746fee6da03ab592
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-03-07 18:36:59 +0100

    Version 5
    Declarative programming - optimizations.

--- test.php
+++ test.php
@@ -2,34 +2,26 @@
 
 declare(strict_types=1);
 
-function divisible(int $a): Closure {
-    return function (int $b) use ($a): bool {
-        return 0 === $a % $b;
-    };
-}
+$divisible = fn (int $a) => fn (int $b): bool => 0 === $b % $a;
 
 $input = range(1, 100);
+
 $rules = [
     [
-        'when' => 3,
+        'when' => $divisible(3),
         'then' => 'Fizz',
     ],
     [
-        'when' => 5,
+        'when' => $divisible(5),
         'then' => 'Buzz',
     ]
 ];
 
 $output = array_map(
     function (int $i) use ($rules): string {
-        $divisible = divisible($i);
-
         $reduced = array_reduce(
             $rules,
-            function (string $acc, array $rule) use ($divisible): string {
-                return $acc . ($divisible($rule['when']) ? $rule['then'] : '');
-            },
-            ''
+            fn (?string $acc, array $rule): string => $acc . (($rule['when'])($i) ? $rule['then'] : null)
         );
 
         return ('' === $reduced ? (string) $i : $reduced);
