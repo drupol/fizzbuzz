cb73aa89c7c8293d1475921836e4557a70363dea
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-03-08 08:14:06 +0100

    Version 8
    Use PHP8 `match`.
    Careful, this changes a little bit the behavior of the algorithm.

--- test.php
+++ test.php
@@ -9,45 +9,41 @@ $ℕ = function (): Generator {
         yield $n++;
     }
 };
-$divisible = fn (int $a) => fn (int $b) => fn (string $acc): bool => 0 === ($b % $a);
-$accIsEmpty = fn (int $i) => fn (string $acc): bool => $acc === '';
-$addSuffix = fn (string $suffix) => fn (int $i) => fn (string $acc): string => sprintf('%s%s', $acc, $suffix);
-$justReturnTheNumber = fn (int $i) => fn (string $acc): string => sprintf('%s', $i);
+$divisible = fn (int $a) => fn (int $b): bool => 0 === ($b % $a);
+$addSuffix = fn (string $suffix) => fn (int $i): string => $suffix;
+$justReturnTheNumber = fn (int $i): string => sprintf('%s', $i);
 
 $lazy_iterable_map = fn (callable $callable) => function(iterable $iterable) use ($callable): Generator {
     foreach ($iterable as $key => $value) {
         yield $callable($value, $key);
     }
 };
-$ifThenElse = fn (callable $predicate) => fn (callable $then) => fn (callable $else) => fn (int $a) => fn (string $b): string => $predicate($a)($b) ? $then($a)($b) : $else($a)($b);
 
 $rules = [
     [
+        'if' => $divisible(15),
+        'then' => $addSuffix('FizzBuzz'),
+    ],
+    [
         'if' => $divisible(3),
         'then' => $addSuffix('Fizz'),
-        'else' => $addSuffix(''),
     ],
     [
         'if' => $divisible(5),
         'then' => $addSuffix('Buzz'),
-        'else' => $addSuffix(''),
     ],
-    [
-        'if' => $accIsEmpty,
-        'then' => $justReturnTheNumber,
-        'else' => $addSuffix(''),
-    ]
 ];
 
 $input = new LimitIterator($ℕ(), 0, 100);
 
 $fizzBuzz = fn (array $rules) => $lazy_iterable_map(
     fn (int $i): string =>
-        array_reduce(
-            $rules,
-            fn (string $acc, array $rule): string => $ifThenElse($rule['if'])($rule['then'])($rule['else'])($i)($acc),
-            ''
-        )
+        match (true) {
+            $rules[0]['if']($i) => $rules[0]['then']($i),
+            $rules[1]['if']($i) => $rules[1]['then']($i),
+            $rules[2]['if']($i) => $rules[2]['then']($i),
+            default => $justReturnTheNumber($i),
+        }
 );
 
 print implode(PHP_EOL, iterator_to_array($fizzBuzz($rules)($input)));
